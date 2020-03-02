49a9f280eb5e7d0072b3c9d496382ef11d370178
Zachary <zpyingling9559@gmail.com>
2020-03-02 14:08:27 -0500

    getting the database calls done; still need to console.log information but all of the call and errors work correctly.

--- liri.js
+++ liri.js
@@ -7,13 +7,113 @@ const spotify = new Spotify(keys.spotify);
 const fs = require("fs");
 let firstParameter = process.argv[2];
 
-function concertFinder() {}
+function concertFinder() {
+  let artist = process.argv[3];
 
-function spotifySongFinder() {}
+  axios
+    .get(
+      "https://rest.bandsintown.com/artists/" +
+        artist +
+        "/events?app_id=codingbootcamp"
+    )
+    .then(function(response) {
+      console.log(response.data);
+    })
+    .catch(function(error) {
+      console.log("This artist wasn't found.");
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an object that comes back with details pertaining to the error that occurred.
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
+    });
+}
 
-function movieFinder() {}
+function spotifySongFinder() {
+  let querySearch = process.argv[3];
 
-function doWhatItSays() {}
+  if (querySearch === "" || querySearch === undefined) {
+    spotifyCall("The Sign");
+  } else {
+    spotifyCall(querySearch);
+  }
+}
+
+function spotifyCall(querySearch) {
+  // querySearch parameter will always be the track that you are trying to search for
+  spotify.search({ type: "track", query: querySearch, limit: 20 }, function(
+    err,
+    data
+  ) {
+    if (err) {
+      return console.log("Error occurred: " + err);
+    }
+
+    console.log(data);
+  });
+}
+
+function movieFinder() {
+  // Format on node of movie should be in "" or the spaces of the movie need to be plus signs
+  // Finish console loging the necessary information
+  let movieName = process.argv[3];
+
+  axios
+    .get(
+      "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy"
+    )
+    .then(function(response) {
+      // If the axios was successful...
+      // Then log the body from the site!
+      console.log(response.data);
+    })
+    .catch(function(error) {
+      console.log("That movie wasn't found. Try again.");
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an object that comes back with details pertaining to the error that occurred.
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
+    });
+}
+
+function doWhatItSays() {
+  fs.readFile("random.txt", "utf8", function(error, data) {
+    if (error) {
+      return console.log(error);
+    }
+
+    console.log(data);
+
+    // Then split it by commas (to make it more readable)
+    var dataArr = data.split(",");
+
+    // Assumes that the first parameter in the random.txt will always be "spotify-this-song"
+    // If you want to make it for all of the commands make if-else statements to check firstParameter value
+    firstParameter = dataArr[0];
+    spotifyCall(dataArr[1]);
+  });
+}
 
 function commandSelector() {
   if (firstParameter === "concert-this") {
@@ -30,3 +130,5 @@ function commandSelector() {
     );
   }
 }
+
+commandSelector();
